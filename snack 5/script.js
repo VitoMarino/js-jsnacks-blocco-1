1e7a0c561c774d73b45572082a1cbf20bce68823
VitoMarino <marino.vito2001@gmail.com>
2024-05-13 12:35:01 +0200

    snack 5

---
+++ snack 5/script.js
@@ -0,0 +1,14 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all'utente di inserire un numero,
+// se è dispari inseriscilo nell'array.
+
+const arry = [''];
+
+let question = Number.parseInt(prompt= 'Scrivi un numero');
+
+if (question % 2===1)
+        console.log(arry)
+    else {
+        console.log()
+    }
+
