17b01fec348a3b6d956a009ec0b485f9ec2cdef5
VitoMarino <marino.vito2001@gmail.com>
2024-05-14 11:13:05 +0200

    snack 7

---
+++ snack 7/script.js
@@ -0,0 +1,13 @@
+// Fai inserire un numero, che chiameremo N, all'utente. 
+// Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. 
+// Ogni volta che ne crei uno, stampalo.
+
+const array = [];
+
+let N = Number.parseInt(prompt('Inserisci un numero'));
+
+Math.floor(Math.random (100)+ 1);
+
+for (i = 0; i >= 100; i++) {
+    
+}
